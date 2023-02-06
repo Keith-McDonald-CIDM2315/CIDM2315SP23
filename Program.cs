0d2584faeea59bd1111db3dcde04635977132309
Keith-McDonald <123433060+Keith-McDonald@users.noreply.github.com>
2023-02-06 06:41:09 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Homework3part2;
+using System;
+class Pattern {
+  static void Main() {
+       Console.WriteLine("Assign an int value to N: ");
+        int n = int.Parse(Console.ReadLine());
+        
+        for(int i = 0;i<n;i++){
+            
+        for(int j =0;j<n;j++){
+            Console.Write("#");
+        }
+            Console.WriteLine();
+        }
+  }
+}
