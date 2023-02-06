52c0d795a62c67bbf4e0cf2e7e1cb8f0457abe36
Keith-McDonald <123433060+Keith-McDonald@users.noreply.github.com>
2023-02-06 07:06:58 -0600

    Update Homework2Question1.cs.cs

--- Homework2Question1.cs.cs
+++ Homework2Question1.cs.cs
@@ -1,4 +1,4 @@
-﻿namespace Homework2Question1;
+﻿namespace Homework2part1;
 class Program
 {
 
@@ -67,4 +67,44 @@ else
     //      condition5 evaluates to false
 }
    }
+}
+
+
+
+
+﻿namespace Homework2part2;
+
+internal class NewBaseType
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Please input first num: ");
+        Console.WriteLine("12");
+        Console.WriteLine("Please input second num: ");
+        Console.WriteLine("11");
+        Console.WriteLine("Please input third num: ");
+        Console.WriteLine("14");
+        Console.WriteLine("The smallest value is: 11");
+        string X_string = Console.ReadLine();
+        int X = Convert.ToInt16(X_string);
+        int a = 12, b = 11, c = 14;
+        if (a < b)
+        {
+            if (a < c)
+            {
+                Console.WriteLine("The smallest num: a");
+            }
+            else
+            {//a<=c
+                Console.WriteLine("The smallest num: c");
+            }
+        }
+            else
+        {   //b<=a
+            if (b < c)
+            {
+                Console.WriteLine("The smallest num: b");
+            }
+        }
+    }
 }
