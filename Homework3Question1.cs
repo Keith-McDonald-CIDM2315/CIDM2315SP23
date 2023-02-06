66aaa24a065a594f7a3ba0d4d501188b14e7e70e
Keith-McDonald <123433060+Keith-McDonald@users.noreply.github.com>
2023-02-06 06:51:14 -0600

    Update Homework3Question1.cs

--- Homework3Question1.cs
+++ Homework3Question1.cs
@@ -1,4 +1,4 @@
-﻿namespace Homework3;
+﻿namespace Homework3part1;
 using System;
 class HelloWorld {
   static void Main() 
@@ -24,4 +24,28 @@ class HelloWorld {
     }
     
   }
+}
+
+
+﻿namespace Homework3part3;
+using System;
+public class Program {
+	public static void Main() {
+		// get N from user
+		Console.WriteLine("Assign an int value to N:");
+		int N=Convert.ToInt16(Console.ReadLine());
+		// i will run from 0 to N-1, i represent row number
+		for(int i=0;i<N;i++) {
+			// j will run from 0 to N-1, j represent column number
+			for(int j=0;j<N;j++) {
+				// if row number is less than column number print space else print *
+				if(i<j)
+					Console.Write(" ");
+				else
+					Console.Write("*");
+
+			}
+			Console.Write("\n");
+		}
+	}
 }
